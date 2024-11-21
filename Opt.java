09ae6326e777a40d57482ef0ae750be6dc7e8aa0
CodewGPS <poorvik.shrinil@gmail.com>
2024-11-21 12:39:29 +0530

    change to opt.java

--- Opt.java
+++ Opt.java
@@ -3,41 +3,76 @@ import java.util.Scanner;
 
 class OptimalPageReplacement {
     static int pageFaults(int pages[], int n, int capacity) {
+        // If no pages or no capacity, return 0
+        if (pages == null || n == 0 || capacity == 0) return 0;
+
+        // Set to represent current page frames
         HashSet<Integer> s = new HashSet<>(capacity);
+        
+        // Track page faults
         int page_faults = 0;
 
+        // Iterate through all pages
         for (int i = 0; i < n; i++) {
+            // If page not in current set, it's a page fault
             if (!s.contains(pages[i])) {
+                // If set is not full, simply add the page
                 if (s.size() < capacity) {
                     s.add(pages[i]);
-                } else {
-                    int farthest = i, pageToRemove = -1;
-
-                    for (int page : s) {
-                        int j;
-                        for (j = i + 1; j < n; j++) {
-                            if (pages[j] == page) {
-                                if (j > farthest) {
-                                    farthest = j;
-                                    pageToRemove = page;
-                                }
-                                break;
-                            }
-                        }
-                        if (j == n) {
-                            pageToRemove = page;
-                            break;
-                        }
-                    }
-
+                } 
+                // If set is full, need to find page to replace
+                else {
+                    // Find page to replace
+                    int pageToRemove = findOptimalPageToReplace(pages, s, i);
+                    
+                    // Remove the page identified for replacement
                     s.remove(pageToRemove);
+                    
+                    // Add current page
                     s.add(pages[i]);
                 }
+                
+                // Increment page faults
                 page_faults++;
             }
         }
 
         return page_faults;
+    }
+
+    // Method to find optimal page to replace
+    static int findOptimalPageToReplace(int pages[], HashSet<Integer> s, int currentIndex) {
+        int farthestPageIndex = -1;
+        int pageToRemove = -1;
+
+        // Check each page in current set
+        for (int page : s) {
+            // Find next occurrence of this page
+            int nextIndex = findNextOccurrence(pages, page, currentIndex);
+
+            // If page not found in remaining sequence
+            if (nextIndex == -1) {
+                return page;  // Immediately return this page
+            }
+
+            // Update farthest page if this page occurs later
+            if (nextIndex > farthestPageIndex) {
+                farthestPageIndex = nextIndex;
+                pageToRemove = page;
+            }
+        }
+
+        return pageToRemove;
+    }
+
+    // Find next occurrence of a page from current index
+    static int findNextOccurrence(int pages[], int page, int currentIndex) {
+        for (int i = currentIndex + 1; i < pages.length; i++) {
+            if (pages[i] == page) {
+                return i;
+            }
+        }
+        return -1;  // Page not found in remaining sequence
     }
 
     public static void main(String args[]) {
